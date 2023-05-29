21fc70ebf88dcadda872e3c276356ee2d519edb1
Romain <insc.romain@gmail.com>
2023-05-28 21:30:21 -0400

    feat: create service to fetch and add cards

--- src/app/services/cooking-card.service.spec.ts
+++ src/app/services/cooking-card.service.spec.ts
@@ -1,16 +1,59 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CookingCardService } from './cooking-card.service';
+import { HttpClientTestingModule, HttpTestingController  } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+import { cookingCardStub } from 'src/stubs/cookingCardStub';
 
 describe('CookingCardService', () => {
   let service: CookingCardService;
+  let httpMock: HttpTestingController;
+  let mockCookingCardsUrl = 'http://localhost:3000/api/v1/cooking-cards'
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ 
+        HttpClientTestingModule,
+    ]
+    });
     service = TestBed.inject(CookingCardService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
+
+  afterAll(() => {
+    httpMock.verify();
+  })
+
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should get all cards', () => {
+    const testData = cookingCardStub;  
+
+    service.getCookingCards().subscribe(data =>
+      expect(data).toEqual(testData)
+    );
+
+    const req = httpMock.expectOne(request => 
+      request.method === 'GET' &&
+      request.url === mockCookingCardsUrl  
+    );
+
+    req.flush(testData);
+  })
+
+  it('should add new card', () => {
+    const testData = cookingCardStub;
+
+    service.addCookingCard().subscribe()
+
+    const req = httpMock.expectOne(request => 
+      request.method === 'POST' &&
+      request.url === mockCookingCardsUrl  
+    ); 
+
+    req.flush(testData);
+  })
 });
--- src/app/services/cooking-card.service.ts
+++ src/app/services/cooking-card.service.ts
@@ -1,16 +1,30 @@
 import { Injectable } from '@angular/core';
 import { ICookingCard } from '../interfaces/cooking-card.interface';
-import { Observable, of } from 'rxjs'
-import { cookingCardStub } from 'src/stubs/cookingCardStub';
+import { Observable, catchError, of, throwError } from 'rxjs'
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class CookingCardService {
 
-  constructor() { }
+    constructor(
+        private http: HttpClient
+    ) { }
 
-  getCookingCards(): Observable<ICookingCard[]>{
-    return of(cookingCardStub)
-  }
+    getCookingCards(): Observable<ICookingCard[]> {
+        return this.http.get<any>('http://localhost:3000/api/v1/cooking-cards').pipe(
+            catchError((err: HttpErrorResponse) => {
+                return throwError(() => err)
+            })
+        )
+    }
+
+    addCookingCard(card: ICookingCard) {
+        return this.http.post('http://localhost:3000/api/v1/cooking-cards', card).pipe(
+            catchError((err: HttpErrorResponse) => {
+                return throwError(() => err)
+            })
+        )
+    }
 }
