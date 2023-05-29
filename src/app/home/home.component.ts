7f3734edc8a2600dba203c28c02c43f028d16089
Romain <insc.romain@gmail.com>
2023-05-28 21:30:49 -0400

    feat: toggle to show card details

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,45 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CookingCardService } from '../services/cooking-card.service';
+import { map } from 'rxjs';
+import { ICookingCard } from '../interfaces/cooking-card.interface';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  cookingCards$ = this.cookingCardService.getCookingCards();
+    cookingCards$ = this.getCookingCards();
+    showCardDetails = false;
 
-  constructor(
-    public cookingCardService: CookingCardService
-  ){};
+    constructor(
+        public cookingCardService: CookingCardService
+    ) { };
 
+    ngOnInit() {}
 
+    getCookingCards() {
+        return this.cookingCardService.getCookingCards().pipe(
+            map(cards => cards.map((card: ICookingCard) => {
+                return { ...card, showList: false }
+            }))
+        )
+    }
 
+    toggleShowCardDetails(id: number | undefined, currentShowList: boolean) {
+        this.cookingCards$ = this.cookingCardService.getCookingCards().pipe(
+            map(cards =>
+                cards.map((card: ICookingCard) => {
+                    if (card.id === id) {
+                        return { ...card, showList: !currentShowList };
+                    } else {
+                        return { ...card, showList: false };
+                    }
+                })
+            )
+        );
+    }
 }
